27b4b1380eb9c4f4151ea135724979f50a5d482f
Ryan Thomas <44555719+rthomas24@users.noreply.github.com>
2020-04-25 14:59:05 -0400

    Update blackjack.py
    Added documentation for functions and added a loop to keep the game running until user wants to quit

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,4 @@
-# Black Jack game for group James Dwyer, Andre Medeiros, Elton Dias,
-# Rony Lopes, Ryan Thomas, Demeus Alves, Sourivong Thepsimoung
+
 
 # pygame and pydealer
 # import pygame
@@ -7,6 +6,7 @@ import pydealer
 from stack import Stack
 from pydealer.const import POKER_RANKS
 import sys
+
 
 # initialize the pygame library
 # pygame.init()
@@ -22,9 +22,8 @@ class Actions:
         self.bet = bet
         self.deal = deal
         self.total = total
-      
         return
-    
+
 
 # money class to keep track of player earnings
 class Money:
@@ -32,8 +31,12 @@ class Money:
         self.total = total
         self.add = add
         self.subtract = subtract
+'''
+This function takes in the strings of the two cards in the user and dealers hands. It determines if it is a face card,
+ace, or just a normal number and computes the two cards values together to create hand values.
 
-
+hand: string representations of the cards
+'''
 def sum_of_card(hand):
     sum = 0
     for card in hand:
@@ -48,36 +51,36 @@ def sum_of_card(hand):
     # print("\n The value of the hand is ", sum)
     return sum
 
+'''
+This function computes if the player or dealer first has blackjack, if not we are also passing in a hand when there is a chance a hand could be busted.
 
+hand: player or dealers hand value
+'''
 def blackjack_or_bust(hand):
     if hand == 21:
         print("Winner")
-        quit()
+        play_again = input("\nDo you want to play again: 'y' or 'n':\n").lower()
+        if play_again == 'y':
+            game()
     elif hand > 21:
-        return True
-    else:
-        return False
-
+        return
 
 '''
-# check if current hand is busted
-def bust_check(hand) -> bool:
-    if hand > 21:
-        return True
-    else:
-        return False
-'''
+This function is called after the user decides they want to stand. A dealer must hit until they have a value of at least 17.
 
-# dealer action
+dealers hand: holds the cards strings representation
+dealers hand value: int of hands value
+stack: current stack that is being used to add cards from
+'''
 def dealer_actions(dealers_hand, dealers_hand_value, stack):
     while (dealers_hand_value < 17):
         dealers_hand.add(stack.deal(1))
         dealers_hand_value = sum_of_card(dealers_hand)
-    print("The dealers hand is now:\n", dealers_hand)
-    print("The dealers hand value is now:\n", dealers_hand_value)
+    print("\nThe dealers hand is now:\n", dealers_hand)
+    print("\nValue =", dealers_hand_value)
     return dealers_hand_value
 
-def main():
+def game():
     stack = pydealer.Stack()  # creating stack variable and = to pydealers stack
     deck = pydealer.Deck(rank=POKER_RANKS, rebuild=True,
                          re_shuffle=True)  # creating deck variable and = to pydealers deck of 52 cards
@@ -92,21 +95,18 @@ def main():
 
     print("Players hand is:\n", players_hand)
     players_hand_value = sum_of_card(players_hand)  # function returns sum which contains the value
-    print("The value of the players hand is ", players_hand_value)
+    print("Value =", players_hand_value)
 
-    print("\n", "Dealer hand is:\n", dealer_hand[1])
     dealer_hand_value = sum_of_card(dealer_hand)
+    if dealer_hand_value == 21:
+        print("\nDealer wins the hand\n")
+        print(dealer_hand)
+        print("Value:", dealer_hand_value)
+        return
+    print("\n", "Dealer hand is:\n", dealer_hand[1])
 
     # Black Jack Check
     blackjack_or_bust(players_hand_value)
-
-    # Check here for dealers 21 because if it has 21 we need to print out dealers value and cards as well
-    if dealer_hand_value == 21:
-        print("Dealer wins the hand")
-        print("The dealers cards are", dealer_hand)
-        print("The dealers value is", dealer_hand_value)
-
-    # loop to play the game
 
     pick_action = input("\nWould you like to hit ('h'), stand ('s') or double down ('d'): ").lower()
 
@@ -114,45 +114,53 @@ def main():
 
         if pick_action == 'h':
 
-            # create hit function instead so we can pass in whoevers turn it is
             players_hand.add(stack.deal(1))
             players_hand_value = sum_of_card(players_hand)
             print(players_hand)
             print("\n The value of the hand is ", players_hand_value)
             blackjack_or_bust(players_hand_value)
 
-            # hand_value = add_card(players_hand, stack)
-
-            # check the cards value
             if blackjack_or_bust(players_hand_value) == True:
                 print("Player has busted")
                 return
             else:
                 pick_action = input("\nWould you like to hit ('h'), stand ('s') or double down ('d'): ").lower()
-
         elif pick_action == 'q':
-            sys.quit()
-
+            sys.exit()
         elif pick_action == 's':
             if players_hand_value < 17 and dealer_hand_value >= 17:
+                print("\nThe dealers hand is now:\n", dealer_hand)
+                print("\nValue =", dealer_hand_value)
                 print("You lose")
                 return
             else:
                 dealer_hand_value = dealer_actions(dealer_hand, dealer_hand_value, stack)
                 if blackjack_or_bust(dealer_hand_value) == True:
-                    print("Dealer has busted")
+                    print("\nDealer has busted")
                     return
 
                 if (dealer_hand_value > players_hand_value):
-                    print("Dealer wins")
-
+                    print("\nDealer wins")
                 elif (players_hand_value > dealer_hand_value):
-                    print("Player wins")
-
+                    print("\nPlayer wins")
                 elif (dealer_hand_value == players_hand_value):
-                    print("The hand is Pushed")
-
+                    print("\nThe hand is Pushed")
                 return
+
+
+
+
+def main():
+    game()
+    play_again = input("\nDo you want to play again: 'y' or 'n':\n").lower()
+    while(play_again != "n"):
+        game()
+        play_again = input("\nDo you want to play again: 'y' or 'n':\n").lower()
+
+    return
+
+
+
 
 if __name__ == "__main__":
     main()
